855fd00edb2871cb5aaa18e043906d265bb79923
Rajat Singh <rajatsingh.b115@gmail.com>
2021-08-31 00:49:44 +0530

    adding .sh file

---
+++ CSZ208507.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+module load compiler/gcc/7.1.0/compilervars
+
+if [ "$#" -eq 4 ]
+then
+	if [ "$1" == "-apriori" ]
+	then
+		./part2a/apriori "$2" "$3" "$4"
+	fi
+	
+	if [ "$1" == "-prefixspan" ]
+	then
+		./part3/prefixSpan "$2" "$3" "$4"
+	fi
+fi
+
+if [ "$#" -eq 3 ]
+then
+	if [ "$2" == "-plot" ]
+	then
+		python3 plot.py "$1"
+	fi
+fi
