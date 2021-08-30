855a8b199a7eba484ff40786ad3fdf964fe30545
Rajat Singh <rajatsingh.b115@gmail.com>
2021-08-31 00:50:28 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+module load compiler/gcc/7.1.0/compilervars
+
 g++ -o part2a/apriori part2a/apriori.cpp
 g++ -o part3/prefixSpan part3/prefixSpan.cpp
-
-module load compiler/gcc/7.1.0/compilervars
 
 cd part2b/fpgrowth/fpgrowth/src/
 make all
