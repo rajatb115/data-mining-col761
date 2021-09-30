4f3a70dde5fa0f10397439f93b857cd782b0b781
whywasievenhere <67458343+whywasievenhere@users.noreply.github.com>
2021-09-30 13:22:12 +0530

    Add files via upload

---
+++ Assignment 2/Part 1/gaston-1.1-re/subgraphiso.cpp
@@ -0,0 +1,16 @@
+// subgraphiso.cpp
+// Siegfried Nijssen, snijssen@liacs.nl, jul 2004.
+#include "subgraphiso.h"
+#include <cstring>
+
+
+SubGraphIso::SubGraphIso()
+{
+}
+
+
+SubGraphIso::~SubGraphIso()
+{
+}
+
+
