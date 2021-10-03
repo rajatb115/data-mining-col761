2f2ac700995f49c02e94f339ef1e5ac9475ff2fc
Rajat Singh <csz208507@chas104.hpc.iitd.ac.in>
2021-10-04 00:56:36 +0530

    adding plot.pyy

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module load compiler/gcc/7.1.0/compilervars
+
+g++ -o Q1/FSG/script_fsg Q1/FSG/script_fsg.cpp
---
+++ plot.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import subprocess
+import time
+import sys
+import os
+
+# Thresholds in %
+threshold = [5,10,25,50,95]
+
+# Start time for each Threshold
+start_time_fsg = []
+# start_time_ = []
+# start_time_ = []
+
+# End time for each Threshold
+end_time_fsg = []
+# end_time_ = []
+# end_time_ = []
+
+# Preprocessing of the data file for FSG
+print("Processing the file for FSG")
+print("./Q1/FSG/script_fsg",sys.argv[1],"data_file.txt")
+subprocess.run(["./Q1/FSG/script_fsg",sys.argv[1],"/Q1/FSG/data_file.txt"])
+print("Processing of the file completed")
+
+
+for i in threshold:
+	start_time_fsg.append(time.time())
+	subprocess.run(["./Q1/FSG/fsg","-s",str(i),"/Q1/FSG/data_file.txt"])
+	end_time_fsg.append(time.time())
+
+'''
+for i in threshold2:
+	start_time_apriori.append(time.time())
+	subprocess.run(["./part2a/apriori",sys.argv[1],str(i),"output/output_apriori_"+str(i)+".txt"])
+	end_time_apriori.append(time.time())
+
+'''
+time_fsg=[]
+
+for i in range(len(threshold2)):
+	time_fsg.append(end_time_fsg[i] - start_time_sfg[i])
+
+
+plt.plot(threshold,time_fsg,'-r',label = 'FSG')
+
+plt.xlabel('Support threshold %')
+plt.ylabel('Running time (sec)')
+
+plt.legend(loc='upper right')
+plt.title('Support threshold vs running time for FSG')
+
+plt.savefig(sys.argv[2])
