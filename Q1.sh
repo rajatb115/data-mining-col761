117fc786ef76a2c462bb4d764611e114f0a0654d
Rajat Singh <csz208507@login04.hpc.iitd.ac.in>
2021-10-04 16:42:24 +0530

    fsg plot

--- Q1.sh
+++ Q1.sh
@@ -3,7 +3,4 @@
 chmod 777 compile.sh
 ./compile.sh
 
-a=`pwd`
-echo $a
-
 python3 plot.py "$1" "$2.png"
--- plot.py
+++ plot.py
@@ -19,14 +19,13 @@ end_time_fsg = []
 
 # Preprocessing of the data file for FSG
 print("Processing the file for FSG")
-print("./Q1/FSG/script_fsg",str(sys.argv[1]),"/Q1/FSG/data_file.txt")
-subprocess.run(["./Q1/FSG/script_fsg",sys.argv[1],"/Q1/FSG/data_file.txt"])
+subprocess.run(["./Q1/FSG/script_fsg",sys.argv[1],"Q1/FSG/data_file.txt"])
 print("Processing of the file completed")
 
 
 for i in threshold:
 	start_time_fsg.append(time.time())
-	subprocess.run(["./Q1/FSG/fsg","-s",str(i),"/Q1/FSG/data_file.txt"])
+	subprocess.run(["./Q1/FSG/fsg","-s",str(i),"Q1/FSG/data_file.txt"])
 	end_time_fsg.append(time.time())
 
 '''
