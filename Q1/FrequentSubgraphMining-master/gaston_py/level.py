c18d7c24edc856617922ad163d13817920490df8
Rajat Singh <rajatsingh.b115@gmail.com>
2021-10-05 21:13:48 +0530

    deleted extra file

--- Q1/FrequentSubgraphMining-master/gaston_py/level.py
+++
@@ -1,9 +0,0 @@
-
-from enum import IntEnum
-
-class Level(IntEnum):
-    """ Constants specifying the queue corresponding to the graph type. """
-    NODE = 0
-    PATH = 1
-    TREE = 2
-    CYCLE = 3
