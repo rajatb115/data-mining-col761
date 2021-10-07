fef023219819123ddfb8aa5d11a256562553b21d
Rajat Singh <rajatsingh.b115@gmail.com>
2021-10-07 11:51:01 +0530

    minor change

--- install.sh
+++ install.sh
@@ -11,11 +11,18 @@ cd "$folder"
 # unzip "$file"
 
 # loading relevent version of compilers
-module load compiler/gcc/7.1.0/compilervars
+# module load compiler/gcc/7.1.0/compilervars
 # module load compiler/python/3.6.0/ucs4/gnu/447
-module load compiler/intel/2019u5/intelpython3
+# module load compiler/intel/2019u5/intelpython3
 
 # loading relevent libraries
+# module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
+# module load pythonpackages/3.6.0/scipy/1.1.0/gnu
+# module load pythonpackages/3.6.0/numpy/1.16.1/gnu
+
+module load compiler/gcc/7.1.0/compilervars
+# module load compiler/intel/2019u5/intelpython3
 module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
 module load pythonpackages/3.6.0/scipy/1.1.0/gnu
+module load compiler/intel/2019u5/intelpython3
 module load pythonpackages/3.6.0/numpy/1.16.1/gnu
