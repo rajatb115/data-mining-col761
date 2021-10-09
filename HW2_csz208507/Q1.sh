4c8c426efa24068f80f54c02a689e1f182bd0b0a
Rajat Singh <rajatsingh.b115@gmail.com>
2021-10-09 14:54:38 +0530

    zip

--- HW2_csz208507/Q1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod 777 compile.sh
-./compile.sh
-
-python3 plot.py "$1" "$2.png"
--- HW2_csz208507/Q2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod 777 compile.sh
-./compile.sh
-
-python3 Q2/part2.py "$1" "$2.png"
