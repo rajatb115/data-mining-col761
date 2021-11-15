29aea270afc6706309a8926f4a8711798722ebef
Rajat Singh <rajatsingh.b115@gmail.com>
2021-11-15 16:02:29 +0530

    arranging

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# clone the repository from github
+repo="https://github.com/rajatb115/data-mining-col761.git"
+git clone "$repo"
+
+folder="data-mining-col761"
+cd "$folder"
+
+file="HW3_csz208507.zip"
+unzip "$file"
+
+module load compiler/gcc/7.1.0/compilervars
+module load compiler/intel/2019u5/intelpython3
