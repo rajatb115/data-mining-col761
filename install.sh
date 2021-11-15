a576c3a9353a3dd9ce0575849b46a767fade7df8
Rajat Singh <csz208507@chas053.hpc.iitd.ac.in>
2021-11-15 17:29:52 +0530

    Q1.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,10 @@ cd "$folder"
 file="HW3_csz208507.zip"
 unzip "$file"
 
+cd "HW3_csz208507"
+
+chmod 777 "Q2.sh"
+chmod 777 "Q1.sh"
+
 module load compiler/gcc/7.1.0/compilervars
 module load compiler/intel/2019u5/intelpython3
